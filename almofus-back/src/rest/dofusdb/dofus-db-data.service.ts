57dfb5071a050e595a9895187b2a8a8bb7389c14
Justine Coulon <justinecoulon1@gmail.com>
2025-01-24 07:04:10 +0100

    fixed typo in dofusdbdto and started adding mapping from dofusdbQuestDto to quest entity

--- almofus-back/src/rest/dofusdb/dofus-db-data.service.ts
+++ almofus-back/src/rest/dofusdb/dofus-db-data.service.ts
@@ -2,14 +2,59 @@ import { Injectable } from '@nestjs/common';
 import { AlmanaxQuestRepository } from 'src/db/repositories/almanax-quest/almanax-quest.repository';
 import { ItemRepository } from 'src/db/repositories/item/item.repository';
 import { LabelRepository } from 'src/db/repositories/label/label.repository';
-import { DofusDbAlmanaxBonusDto, DofusDbItemDto, DofusDbNpcDto, DofusDbQuestDto, SyncRequestDto } from './dto/dofus-db.dto';
+import {
+  DofusDbAlmanaxBonusDto,
+  DofusDbItemDto,
+  DofusDbNpcDto,
+  DofusDbQuestDto,
+  SyncRequestDto,
+} from './dto/dofus-db.dto';
 import { AlmanaxQuest } from 'src/db/model/almanax-quest.entity';
+import { Item } from 'src/db/model/item.entity';
 
 @Injectable()
 export class DofusDbDataService {
-    constructor(private readonly itemRepository: ItemRepository,
-        private readonly almanaxQuestRepository: AlmanaxQuestRepository,
-        private readonly labelRepository: LabelRepository
-    ) { }
+  private dofusDbNpcDtos: DofusDbNpcDto[] = [];
+  private dofusDbItemDtos: DofusDbItemDto[] = [];
+  private dofusDbAlmanaxBonusDtos: DofusDbAlmanaxBonusDto[] = [];
+  private dofusDbQuestDto: DofusDbQuestDto[] = [];
 
+  constructor(
+    private readonly itemRepository: ItemRepository,
+    private readonly almanaxQuestRepository: AlmanaxQuestRepository,
+    private readonly labelRepository: LabelRepository,
+  ) {}
+
+  mapDofusDbDataToEntities(syncRequestDto: SyncRequestDto) {
+    this.dofusDbNpcDtos = syncRequestDto.dofusDbNpcDtos;
+    this.dofusDbItemDtos = syncRequestDto.dofusDbItemDtos;
+    this.dofusDbAlmanaxBonusDtos = syncRequestDto.dofusDbAlmanaxBonusDtos;
+    this.dofusDbQuestDto = syncRequestDto.dofusDbQuestDto;
+
+    const almanaxQuestEntities = this.dofusDbQuestDto.map((questDto) =>
+      this.mapQuestDtoToEntity(questDto),
+    );
+  }
+
+  mapQuestDtoToEntity(questDto: DofusDbQuestDto): AlmanaxQuest {
+    const dofusDbItemDto: DofusDbItemDto = this.findItemById(
+      questDto.steps.objectives[0].need.generated.items[0],
+    );
+    const item: Item = {
+      dofusId: dofusDbItemDto.id,
+      level: dofusDbItemDto.level,
+      nameLabel: dofusDbItemDto.nameLabelDto,
+    };
+
+    return {
+      itemQuantity: questDto.steps.objectives[0].need.generated.quantities[0],
+      dofusNpcId: questDto.steps.objectives[3].parameters.parameter0,
+      kamaReward: questDto.steps.rewards[0].kamaRatio,
+      item: item,
+    };
+  }
+
+  findItemById(id: number): DofusDbItemDto {
+    return this.dofusDbItemDtos.find((item) => item.id === id);
+  }
 }
--- almofus-back/src/rest/dofusdb/dto/dofus-db.dto.ts
+++ almofus-back/src/rest/dofusdb/dto/dofus-db.dto.ts
@@ -1,54 +1,54 @@
 type LabelDto = {
-    id: string;
-    en: string;
-    pt: string;
-    es: string;
-    fr: string;
+  id: string;
+  en: string;
+  pt: string;
+  es: string;
+  fr: string;
 };
 
 type Objective = {
-    parameters: {
-        parameter0: number;
-    },
-    need: {
-        generated: {
-            items: [id: number],
-            quantities: [6]
-        }
-    },
-    className: string;
-}
+  parameters: {
+    parameter0: number;
+  };
+  need: {
+    generated: {
+      items: number[];
+      quantities: number[];
+    };
+  };
+  className: string;
+};
 
 export type DofusDbNpcDto = {
-    id: number;
-    nameLabelDto: LabelDto;
+  id: number;
+  nameLabelDto: LabelDto;
 };
 
 export type DofusDbItemDto = {
-    id: number;
-    level: number;
-    nameLabelDto: LabelDto;
+  id: number;
+  level: number;
+  nameLabelDto: LabelDto;
 };
 
 export type DofusDbAlmanaxBonusDto = {
-    npcId: number;
-    nameLabelDto: LabelDto;
-    img: string;
-    descLabelDto: LabelDto;
+  npcId: number;
+  nameLabelDto: LabelDto;
+  img: string;
+  descLabelDto: LabelDto;
 };
 
 export type DofusDbQuestDto = {
-    id: number;
-    nameLabelDto: LabelDto;
-    steps: {
-        rewards: [{ kamaRatio: number; }],
-        objectives: Objective[]
-    };
+  id: number;
+  nameLabelDto: LabelDto;
+  steps: {
+    rewards: [{ kamaRatio: number }];
+    objectives: Objective[];
+  };
 };
 
 export type SyncRequestDto = {
-    dofusDbNpcDtos: DofusDbNpcDto[],
-    dofusDbItemDtos: DofusDbItemDto[],
-    dofusDbAlmanaxBonusDtos: DofusDbAlmanaxBonusDto[],
-    dofusDbQuestDto: DofusDbQuestDto[],
+  dofusDbNpcDtos: DofusDbNpcDto[];
+  dofusDbItemDtos: DofusDbItemDto[];
+  dofusDbAlmanaxBonusDtos: DofusDbAlmanaxBonusDto[];
+  dofusDbQuestDto: DofusDbQuestDto[];
 };
