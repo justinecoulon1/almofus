d4c0a24894cb0bc77a7e4508ecc81efdc32616cb
Justine Coulon <justinecoulon1@gmail.com>
2025-02-19 10:15:22 +0100

    fixed must log in section

--- almofus-front/src/components/global/must-log-in/must-log-in-section.tsx
+++ almofus-front/src/components/global/must-log-in/must-log-in-section.tsx
@@ -1,7 +1,19 @@
 import { useTranslations } from 'next-intl';
+import Image from 'next/image';
 import styles from './must-log-in-section.module.css';
 
 export function MustLogInSection() {
   const t = useTranslations('mustlogin');
-  return <div className={styles.loginLightBoxContainer}></div>;
+  return (
+    <div className={styles.mustLogInSectionContainer}>
+      <Image
+        className={styles.mustLogInImage}
+        src={'/icons/restriction.png'}
+        alt={'must be logged in'}
+        width={512}
+        height={512}
+      />
+      <h2 className={styles.errorMessage}>{t('errorMessage')}</h2>
+    </div>
+  );
 }
