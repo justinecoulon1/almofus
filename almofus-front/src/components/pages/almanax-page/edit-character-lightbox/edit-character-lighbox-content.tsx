2c9a727ca666307b4cf0cf14234b0680f495a47e
Admin Stagiaire <justinecoulon1@gmail.com>
2025-02-25 12:53:14 +0100

    added character name input to edit character lightbox

--- almofus-front/src/components/pages/almanax-page/edit-character-lightbox/edit-character-lighbox-content.tsx
+++ almofus-front/src/components/pages/almanax-page/edit-character-lightbox/edit-character-lighbox-content.tsx
@@ -1,10 +1,28 @@
 'use client';
 
 import { useTranslations } from 'next-intl';
+import styles from './edit-character-lightbox-content.module.css';
+import classNames from 'classnames';
+import { useState } from 'react';
+import { SecondaryButtonWithImage, SecondaryDarkButtonWithImage } from '@/components/generic/buttons/button-img';
 
 export function EditCharacterLightboxContent() {
-    const t = useTranslations('almanax-page');
+    const t = useTranslations('edit-character-lightbox');
+    const [isEditable, setIsEditable] = useState(false);
     return (
-        <div>  </div>
+        <div className={styles.lightboxContainer} onMouseDown={(e) => e.stopPropagation()}>
+            <div className={styles.characterNameContainer}>
+                <input type="text" className={classNames(styles.nameInput, { [styles.editable]: !isEditable })} />
+                <SecondaryButtonWithImage
+                    className={styles.editButton} imageStyle={styles.editImage} imageSrc='/icons/edit.png' imageAlt='edit' onClick={() => {
+                        if (isEditable) {
+                            setIsEditable(false);
+                        } else {
+                            setIsEditable(true)
+                        }
+                    }
+                    } />
+            </div>
+        </div >
     );
 }
