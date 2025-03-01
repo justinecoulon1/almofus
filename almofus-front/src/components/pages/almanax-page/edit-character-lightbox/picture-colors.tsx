aa868a7c2d22f17f6313e36bb7c67e50800deead
Justine Coulon <justinecoulon1@gmail.com>
2025-03-01 14:45:12 +0100

    refactored color buttons in edit lightbox

--- almofus-front/src/components/pages/almanax-page/edit-character-lightbox/picture-colors.tsx
+++ almofus-front/src/components/pages/almanax-page/edit-character-lightbox/picture-colors.tsx
@@ -1,41 +1,21 @@
 import styles from './picture-colors.module.css';
-import classNames from 'classnames';
 import { useEditCharacterLightboxContext } from '@/components/pages/almanax-page/edit-character-lightbox/edit-character-lightbox-context';
+
+const colors = ['white', 'red', 'grey', 'blue', '#abcd00', 'yellow', 'orange'];
 
 export function PictureColorsSection() {
   const { setProfilePictureColor } = useEditCharacterLightboxContext();
   return (
     <div className={styles.colorsDiv}>
-      <button
-        value="transparent"
-        className={classNames(styles.colorButton, styles.transparentButton)}
-        onClick={(e) => setProfilePictureColor(e.currentTarget.value)}
-      ></button>
-      <button
-        value="red"
-        className={classNames(styles.colorButton, styles.redButton)}
-        onClick={(e) => setProfilePictureColor(e.currentTarget.value)}
-      ></button>
-      <button
-        value="grey"
-        className={classNames(styles.colorButton, styles.greyButton)}
-        onClick={(e) => setProfilePictureColor(e.currentTarget.value)}
-      ></button>
-      <button
-        value="blue"
-        className={classNames(styles.colorButton, styles.blueButton)}
-        onClick={(e) => setProfilePictureColor(e.currentTarget.value)}
-      ></button>
-      <button
-        value="green"
-        className={classNames(styles.colorButton, styles.greenButton)}
-        onClick={(e) => setProfilePictureColor(e.currentTarget.value)}
-      ></button>
-      <button
-        value="yellow"
-        className={classNames(styles.colorButton, styles.yellowButton)}
-        onClick={(e) => setProfilePictureColor(e.currentTarget.value)}
-      ></button>
+      {colors.map((color, index) => (
+        <button
+          value={color}
+          key={index}
+          className={styles.colorButton}
+          onClick={() => setProfilePictureColor(color)}
+          style={{ backgroundColor: color }}
+        />
+      ))}
     </div>
   );
 }
